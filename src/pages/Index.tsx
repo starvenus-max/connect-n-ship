7553519f7cdc925b2c3be67a852c43fb86c4c17e
starvenus-max <internstar.venus@gmail.com>
2026-03-06 10:59:14 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
-import StatsBar from "../components/StatsBar";
 import ServicesSection from "../components/ServicesSection";
 import AboutSection from "../components/AboutSection";
 import ContactSection from "../components/ContactSection";
@@ -11,7 +10,6 @@ const Index = () => {
     <>
       <Header />
       <HeroSection />
-      <StatsBar />
       <ServicesSection />
       <AboutSection />
       <ContactSection />
